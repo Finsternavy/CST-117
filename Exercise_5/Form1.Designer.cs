1537fd114ea0c00741fb162c7dea22987f1b53f4
Christopher Finster <finsternavy@gmail.com>
2021-03-21 13:30:08 -0500

    Exercise 5 upload
    Exercise 5 upload

---
+++ Exercise_5/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Exercise_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.termsLabel = new System.Windows.Forms.Label();
+            this.termsTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.approximateInputLabel = new System.Windows.Forms.Label();
+            this.piEqualsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // termsLabel
+            // 
+            this.termsLabel.AutoSize = true;
+            this.termsLabel.Location = new System.Drawing.Point(41, 47);
+            this.termsLabel.Name = "termsLabel";
+            this.termsLabel.Size = new System.Drawing.Size(85, 13);
+            this.termsLabel.TabIndex = 0;
+            this.termsLabel.Text = "Enter # of terms:";
+            // 
+            // termsTextBox
+            // 
+            this.termsTextBox.Location = new System.Drawing.Point(154, 44);
+            this.termsTextBox.Name = "termsTextBox";
+            this.termsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.termsTextBox.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(44, 85);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(90, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "CALCULATE";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // approximateInputLabel
+            // 
+            this.approximateInputLabel.AutoSize = true;
+            this.approximateInputLabel.Location = new System.Drawing.Point(41, 133);
+            this.approximateInputLabel.Name = "approximateInputLabel";
+            this.approximateInputLabel.Size = new System.Drawing.Size(120, 13);
+            this.approximateInputLabel.TabIndex = 0;
+            this.approximateInputLabel.Text = "Approximate value of pi ";
+            // 
+            // piEqualsLabel
+            // 
+            this.piEqualsLabel.AutoSize = true;
+            this.piEqualsLabel.Location = new System.Drawing.Point(41, 165);
+            this.piEqualsLabel.Name = "piEqualsLabel";
+            this.piEqualsLabel.Size = new System.Drawing.Size(16, 13);
+            this.piEqualsLabel.TabIndex = 0;
+            this.piEqualsLabel.Text = "= ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 275);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.termsTextBox);
+            this.Controls.Add(this.piEqualsLabel);
+            this.Controls.Add(this.approximateInputLabel);
+            this.Controls.Add(this.termsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Approximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label termsLabel;
+        private System.Windows.Forms.TextBox termsTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label approximateInputLabel;
+        private System.Windows.Forms.Label piEqualsLabel;
+    }
+}
+
---
+++ Exercise_5/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            termsTextBox.BackColor = Color.White;
+
+            // start at 3 for the lowest denominator in the sequence
+            int denominater = 3;
+            // start value at 4
+            double pi = 4;
+            // used to determine if the count is even or odd
+            int counter = 1;
+
+            // decided by user input
+            int terms;
+
+
+            // make sure the user entered a number
+            if (int.TryParse(termsTextBox.Text, out terms))
+            {
+                while (counter < terms)
+                {
+                    //if counter is odd
+                    if(counter % 2 != 0){
+
+                        pi -= Convert.ToDouble(4) / denominater;
+                        counter++;
+                    }
+                    // if counter is even
+                    else
+                    {
+                        pi += Convert.ToDouble(4) / denominater;
+                        counter++;
+                    }
+                    denominater += 2;
+                }
+
+                // set input text to display the number of terms used in the calculation 
+                approximateInputLabel.Text = "Approximate value of pi after " + terms.ToString() + " terms";
+
+                // display the answer
+                piEqualsLabel.Text = "= " + pi.ToString();
+            }
+            else
+            {
+                termsTextBox.Text = "INVALID ENTRY!";
+                termsTextBox.BackColor = Color.Red;
+            }
+
+        }
+    }
+}
