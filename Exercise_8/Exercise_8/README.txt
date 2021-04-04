68e2d830fde6e2454d19070e3f63a1a6289963f0
Christopher Finster <finsternavy@gmail.com>
2021-04-04 13:23:40 -0500

    Exercise_9
    Exercise_9

---
+++ Exercise_8/Exercise_8/README.txt
@@ -0,0 +1,24 @@
+Christopher Finster
+CST-117, Exercise 8
+4 Apr 2021
+
+This exercise is a demonstration of problem 4 of Chapter is in Starting out with Visual C Sharp (4th Edition).
+
+I used a static class (Calories) with 2 overloaded methods:
+  
+     fatCalories(int) 
+     fatCalories(double) 
+     carbCalories(int) 
+     carbCalories(double()
+
+This is a Windows Form application for easy demonstration. 
+
+This program will also display the total calories. 
+
+You may enter the fat grams as an int or a double, and you may enter the carbs grams as an int or a double.
+
+They can be mismatched ( one int and one double).  The program should recognize the data type and perform necessary conversions.
+
+Program also retrievable at:
+
+https://github.com/Finsternavy/CST-117/tree/main/Exercise_8/Exercise_8
---
+++ Exercise_9/Program.cs
@@ -0,0 +1,121 @@
+﻿//Corrected by <Christopher Finster>
+//Cannot free type titles.  You must comment this out to avoid compile errors
+//Program
+//use for IC08
+//Lydia's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
+//Set
+//Describe several debugging techniques and scenarios for their use.
+// You cannot use the using keyword in the midle of code.  They should be at the top and removed if duplicates
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//Lydia's code - find the errors!
+namespace CST117_IC08_console
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                // do not need this else logic. 
+                //else
+                    // this would cause the method to return false after only checking the first index of elements. Needs removed. 
+                    //return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set rhs)
+        {
+            // Add a new set to hold the values of both sets you want to union
+            Set joinedSet = new Set();
+            //Copy original elements to the new tempory set
+            joinedSet.elements = this.elements.ToList();
+
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                // the direction state the original set should be unchanged. Using "this" will modify the original set. Need to use a new set to hold the values of both.
+                //this.addElement(rhs.elements[i]);
+                joinedSet.addElement(rhs.elements[i]);
+            }
+            // you do not want to return only the values in rhs.  You wont both sets with no duplicates
+            //return rhs;
+            return joinedSet;
+        }
+    }
+}
