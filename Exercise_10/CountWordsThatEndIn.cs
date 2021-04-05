23e3a92d328599528c1267b7fabf3132876210df
Finsternavy <finsternavy@gmail.com>
2021-04-05 13:39:26 -0500

    Exercise 10
    Exercise 10

---
+++ Exercise_10/CountWordsThatEndIn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// Author: Christopher Finster
+// CST-117, Exercise 10
+// 5 Apr 2021
+
+namespace Exercise_10
+{
+    static class CountWordsThatEndIn
+    {
+        // takes two char variables and a string
+        public static int countWordsEndingIn(char char1, char char2, string textFileString)
+        {
+            // initialize the count at 0
+            int count = 0;
+
+            // set local char variables to simpify code
+            char a = Char.ToLower(char1);
+            char b = Char.ToLower(char2);
+
+            // set file string to local string to simplify code
+            string textFileToUpper = textFileString.ToLower();
+
+            // separate string into a char array for easy iteration 
+            char[] characterArray = textFileToUpper.ToCharArray();
+
+            // used to set the index on Console.WriteLine in foreach loop for debugging purposes
+            int indexFinder = 0;
+
+            // print the array and display the index for debugging purposes
+            foreach(char character in characterArray)
+            {
+                // write the char at the current index and its index in the array for debugging purposes
+                Console.WriteLine(character + " index: " + indexFinder);
+                indexFinder++;
+            }
+
+            // start i at 1 to avoid out of bounds when checking backward.
+            // int i = is industry standard in for loops. I dont care if I lose points for this. Train how you fight
+            for (int i = 1; i < characterArray.Length; i++)
+            {
+                // if the current index is not a letter and the previous letter is one you are looking for 
+                if (!Char.IsLetter(characterArray[i]) && characterArray[i - 1].Equals(a) | characterArray[i - 1].Equals(b))
+                {
+                    // print the last letter to ensure it is one you are looking for, the current index to ensure it is not a letter, and 
+                    // the return of !Char.IsLetter() to ensure it is false for debugging purposes
+                    Console.WriteLine(characterArray[i - 1] + " at index " + (i - 1) + " is followed by " + characterArray[i] + " " +
+                        Char.IsLetter(characterArray[i]));
+
+                    // Increment count as a word ending in a target letter was found
+                    count++;
+                }
+            }
+
+            // return the number of words ending in one of the target letters
+            return count;
+        }
+    }
+}
---
+++ Exercise_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Author: Christopher Finster
+// CST-117, Exercise 10
+// 5 Apr 2021
+
+namespace Exercise_10
+{
+    class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            // Used two char variables for ease of testing different characters
+            char char1 = 'e';
+            char char2 = 't';
+
+            // read in test file... change file directory between "" to change the input file.  Must be a txt file. 
+            string myFile = System.IO.File.ReadAllText(@"C:\Users\Finst\Documents\GitHub\CST-117\Exercise_10\TestFile.txt");
+
+            // set count equal to the return value of the static countWordsEndingIn() method accepts 2 char variables and the file string
+            int count = CountWordsThatEndIn.countWordsEndingIn(char1, char2, myFile);
+
+            // Print out the final count
+            Console.WriteLine("\r\nThere are " + count + " words that end in 'e' or 't'.");
+        }
+    }
+}
